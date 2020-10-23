9d70da18a7a2a16e19c103ef03436d9ad3101cce
worawitboat <worawit.pa.61@ubu.ac.th>
2020-10-23 20:43:08 +0700

    get data to map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,6 +7,7 @@ import { map } from 'rxjs/operators';
 import { AuthService } from '@app/shared/services';
 
 declare const L: any;
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -17,34 +18,49 @@ export class MapComponent implements OnInit {
   mymap = L;
   type = history.state.type;
 
-  food = []
-  noodle = []
-  drink = []
+  food = [
+
+
+  ]
+  noodle = [
+
+  ]
+  drink = [
+    // {
+    //   name: '',
+    //   img: '',
+    //   lat: '',
+    //   lng: '',
+    //   des: '',
+    //   type: ''
+    // }
+  ]
 
   focusImg = {}
   focusName = {}
   focusDes = {}
+  place = []
 
 
   constructor(private location: Location, private authService: AuthService) { }
   ngOnInit() {
-    this.authService.getLocation().subscribe(data => {
-      const place = data;
+    // this.authService.getLocation().subscribe(data => {
+    //   console.log(data);
 
-      // console.log(place);
-      for (let i = 0 ; i < place.length ; i++) {
-        if(place[i].type == 'food'){
-          this.food.push(place[i])
-        }else if(place[i].type == 'noodle'){
-          this.noodle.push(place[i])
-        }else if(place[i].type == 'drink'){
-          this.drink.push(place[i])
-        }
+    //   // console.log(place);
+    //   for (let i in data) {
+    //     if (i['type'] == 'food') {
+    //       this.food.push(i)
+    //     } else if (i['type'] == 'noodle') {
+    //       this.noodle.push(i)
+    //     } else if (i['type'] == 'drink') {
+    //       this.drink.push(i)
+    //     }
 
 
-      }
+    //   }
 
-    });
+    // });
 
     if (!navigator.geolocation) {
       console.log('location is not supported');
@@ -75,81 +91,102 @@ export class MapComponent implements OnInit {
 
 
       let marker = L.marker(latLng).addTo(this.mymap);
-      if (this.type == 'food') {
-        this.ran = Math.floor(Math.random() * this.food.length);
-        console.log(this.food);
+      this.authService.getLocation().subscribe(data => {
 
-        console.log(this.ran);
-        console.log(this.food);
+        console.log(data);
 
-        const focuslatlng = [parseFloat(this.food[this.ran].lat),parseFloat(this.food[this.ran].lng)];
-        this.mymap.flyTo(focuslatlng, 16);
-        this.focusImg = this.food[this.ran].img;
-        this.focusName = this.food[this.ran].name;
-        this.focusDes = this.food[this.ran].des;
-        for (let i = 0; i < this.food.length; i++) {
-          const foodlatlng = [parseFloat(this.food[i].lat),parseFloat(this.food[i].lng)];
-          let store = L.marker(foodlatlng).addTo(this.mymap);
-          store.bindPopup(
-            '<div id="content">' +
-            '<div id="siteNotice">' +
-            "</div>" +
-            '<center>' +
-            '<img src="' + this.food[i].img + '"alt="Girl in a jacket" width="240" height="150">' +
-            '<center/>' +
-            '<h3 id="firstHeading" class="firstHeading">' + this.food[i].name + '</h3>' +
-            '<div id="bodyContent">' +
-            '<h4>' + this.food[i].des + '</h4>' +
-            "</div>" +
-            "</div>")
-
+        // console.log(place);
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].type == 'food') {
+            this.food.push(data[i]);
+          } else if (data[i].type == 'noodle') {
+            this.noodle.push(data[i]);
+          } else if (data[i].type == 'drink') {
+            this.drink.push(data[i]);
+          }
         }
-      } else if (this.type == 'noodle') {
-        this.ran = Math.floor(Math.random() * this.noodle.length);
-        this.mymap.flyTo(this.noodle[this.ran].latlng, 16)
-        this.focusImg = this.noodle[this.ran].img;
-        this.focusName = this.noodle[this.ran].name;
-        this.focusDes = this.noodle[this.ran].des;
-        for (let i = 0; i < this.noodle.length; i++) {
-          let store = L.marker(this.noodle[i].latlng).addTo(this.mymap);
-          store.bindPopup(
-            '<div id="content">' +
-            '<div id="siteNotice">' +
-            "</div>" +
-            '<center>' +
-            '<img src="' + this.noodle[i].img + '"alt="Girl in a jacket" width="240" height="150">' +
-            '<center/>' +
-            '<h3 id="firstHeading" class="firstHeading">' + this.noodle[i].name + '</h3>' +
-            '<div id="bodyContent">' +
-            '<h4>' + this.noodle[i].des + '</h4>' +
-            "</div>" +
-            "</div>")
+
+        if (this.type == 'food') {
+          this.ran = Math.floor(Math.random() * this.food.length);
+          console.log(this.food);
+
+          console.log(this.ran);
+          console.log(this.food);
+
+          const focuslatlng = [parseFloat(this.food[this.ran].lat), parseFloat(this.food[this.ran].lng)];
+          this.mymap.flyTo(focuslatlng, 16);
+          this.focusImg = this.food[this.ran].img;
+          this.focusName = this.food[this.ran].name;
+          this.focusDes = this.food[this.ran].des;
+          for (let i = 0; i < this.food.length; i++) {
+            const foodlatlng = [parseFloat(this.food[i].lat), parseFloat(this.food[i].lng)];
+            let store = L.marker(foodlatlng).addTo(this.mymap);
+            store.bindPopup(
+              '<div id="content">' +
+              '<div id="siteNotice">' +
+              "</div>" +
+              '<center>' +
+              '<img src="' + this.food[i].img + '"alt="Girl in a jacket" width="240" height="150">' +
+              '<center/>' +
+              '<h3 id="firstHeading" class="firstHeading">' + this.food[i].name + '</h3>' +
+              '<div id="bodyContent">' +
+              '<h4>' + this.food[i].des + '</h4>' +
+              "</div>" +
+              "</div>")
+
+          }
+        } else if (this.type == 'noodle') {
+          this.ran = Math.floor(Math.random() * this.noodle.length);
+          const focuslatlng = [parseFloat(this.noodle[this.ran].lat), parseFloat(this.noodle[this.ran].lng)];
+          this.mymap.flyTo(focuslatlng, 16)
+          this.focusImg = this.noodle[this.ran].img;
+          this.focusName = this.noodle[this.ran].name;
+          this.focusDes = this.noodle[this.ran].des;
+          for (let i = 0; i < this.noodle.length; i++) {
+            const noodlelatlng = [parseFloat(this.noodle[i].lat), parseFloat(this.noodle[i].lng)];
+            let store = L.marker(noodlelatlng).addTo(this.mymap);
+            store.bindPopup(
+              '<div id="content">' +
+              '<div id="siteNotice">' +
+              "</div>" +
+              '<center>' +
+              '<img src="' + this.noodle[i].img + '"alt="Girl in a jacket" width="240" height="150">' +
+              '<center/>' +
+              '<h3 id="firstHeading" class="firstHeading">' + this.noodle[i].name + '</h3>' +
+              '<div id="bodyContent">' +
+              '<h4>' + this.noodle[i].des + '</h4>' +
+              "</div>" +
+              "</div>")
+          }
+        } else if (this.type == 'drink') {
+          this.ran = Math.floor(Math.random() * this.drink.length);
+          const focuslatlng = [parseFloat(this.drink[this.ran].lat), parseFloat(this.drink[this.ran].lng)];
+          this.mymap.flyTo(focuslatlng, 16)
+          this.focusImg = this.drink[this.ran].img;
+          this.focusName = this.drink[this.ran].name;
+          this.focusDes = this.drink[this.ran].des;
+          for (let i = 0; i < this.drink.length; i++) {
+            const drinklatlng = [parseFloat(this.drink[i].lat), parseFloat(this.drink[i].lng)];
+            let store = L.marker(drinklatlng).addTo(this.mymap);
+            store.bindPopup(
+              '<div id="content">' +
+              '<div id="siteNotice">' +
+              "</div>" +
+              '<center>' +
+              '<img src="' + this.drink[i].img + '"alt="Girl in a jacket" width="240" height="150">' +
+              '<center/>' +
+              '<h3 id="firstHeading" class="firstHeading">' + this.drink[i].name + '</h3>' +
+              '<div id="bodyContent">' +
+              '<h4>' + this.drink[i].des + '</h4>' +
+              "</div>" +
+              "</div>")
+          }
         }
-      } else if (this.type == 'drink') {
-        this.ran = Math.floor(Math.random() * this.drink.length);
-        this.mymap.flyTo(this.drink[this.ran].latlng, 16)
-        this.focusImg = this.drink[this.ran].img;
-        this.focusName = this.drink[this.ran].name;
-        this.focusDes = this.drink[this.ran].des;
-        for (let i = 0; i < this.drink.length; i++) {
-          let store = L.marker(this.drink[i].latlng).addTo(this.mymap);
-          store.bindPopup(
-            '<div id="content">' +
-            '<div id="siteNotice">' +
-            "</div>" +
-            '<center>' +
-            '<img src="' + this.drink[i].img + '"alt="Girl in a jacket" width="240" height="150">' +
-            '<center/>' +
-            '<h3 id="firstHeading" class="firstHeading">' + this.drink[i].name + '</h3>' +
-            '<div id="bodyContent">' +
-            '<h4>' + this.drink[i].des + '</h4>' +
-            "</div>" +
-            "</div>")
-        }
-      }
 
 
-      marker.bindPopup('<b>Me</b>').openPopup();
+
+        marker.bindPopup('<b>Me</b>').openPopup();
+      });
 
     });
     this.watchPosition();
@@ -158,19 +195,22 @@ export class MapComponent implements OnInit {
   rand() {
     if (this.type == "drink") {
       this.ran = Math.floor(Math.random() * this.drink.length);
-      this.mymap.flyTo(this.drink[this.ran].latlng, 16)
+      const focuslatlng = [parseFloat(this.drink[this.ran].lat), parseFloat(this.drink[this.ran].lng)];
+      this.mymap.flyTo(focuslatlng, 16)
       this.focusImg = this.drink[this.ran].img;
       this.focusName = this.drink[this.ran].name;
       this.focusDes = this.drink[this.ran].des;
     } else if (this.type == "food") {
       this.ran = Math.floor(Math.random() * this.food.length);
-      this.mymap.flyTo(this.food[this.ran].latlng, 16)
+      const focuslatlng = [parseFloat(this.food[this.ran].lat), parseFloat(this.food[this.ran].lng)];
+      this.mymap.flyTo(focuslatlng, 16)
       this.focusImg = this.food[this.ran].img;
       this.focusName = this.food[this.ran].name;
       this.focusDes = this.food[this.ran].des;
     } else if (this.type == "noodle") {
       this.ran = Math.floor(Math.random() * this.noodle.length);
-      this.mymap.flyTo(this.noodle[this.ran].latlng, 16)
+      const focuslatlng = [parseFloat(this.noodle[this.ran].lat), parseFloat(this.noodle[this.ran].lng)];
+      this.mymap.flyTo(focuslatlng, 16)
       this.focusImg = this.noodle[this.ran].img;
       this.focusName = this.noodle[this.ran].name;
       this.focusDes = this.noodle[this.ran].des;
